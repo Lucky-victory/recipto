67388b4bb3e11f0f2b49eb2a6a4e040b9827754e
Lucky Victory <victorylucky96@gmail.com>
2023-06-03 23:35:47 +0000

    added post card comp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -32,6 +32,7 @@ import {
 import { getDevice } from "framework7";
 import "../css/home.scss";
 import isEmpty from "just-is-empty";
+import PostCard from "../components/post-card";
 const HomePage = () => {
   const device = getDevice();
   const isMobile = device.android || device.ios;
@@ -156,118 +157,7 @@ const HomePage = () => {
 
           {posts.map((post, index) => {
             return (
-              <Card key={index} className="rt-card">
-                <CardHeader className="rt-card-header">
-                  <div className="flex jc-center ">
-                    <Link
-                      href="#"
-                      // add link to user profile
-                      className="rt-card-header-link flex jc-center ov-hidden flex-grow"
-                    >
-                      <img
-                        src={post.user?.avatar}
-                        loading="lazy"
-                        alt=""
-                        className="rt-avatar"
-                      />
-                      <div className=" ml-4">
-                        <div className="text-bold rt-card-header-name">
-                          {post.user?.fullname}
-                        </div>
-                        <span className="text-grey text-sm">1h</span>
-                      </div>
-                    </Link>
-                  </div>
-                  <Link href="#" iconOnly>
-                    <Icon
-                      material="more_vert"
-                      className="material-symbols-rounded"
-                    ></Icon>
-                  </Link>
-                </CardHeader>
-
-                    <Block className='text-md'>
-                      {post.text||''}
-                    </Block>
-
-                    {
-                      (post.photo||post.has_recipe )&&
-
-                <CardContent  className="rt-card-content">
-                  <Link href='/about/' className="rt-card-content-inner">
-
-                    <div className="rt-card-img-wrap">
-                      {(isEmpty(post.photo) && post.has_recipe) && (
-                        <div className="rt-img-placeholder">
-                          <span>{post.recipe?.title}</span>
-                        </div>
-                      )}
-                      {post.photo && 
-                        <img
-                          src={post?.photo}
-                          alt=""
-                          loading="lazy"
-                          className="rt-card-img"
-                        />
-                      }
-                      {(isEmpty(post.photo) && post.recipe?.photo) && (
-                        <img hidden
-                          src={post.recipe?.photo}
-                          alt=""
-                          loading="lazy"
-                          className="rt-card-img"
-                        />
-                      )}
-                    </div>
-                    {post.has_recipe && (
-<div className="rt-card-content-footer">
-
-                      
-                      <div className="rt-card-content-title">
-                        <span className="text-bold">{post.recipe?.title}</span>
-                      </div>
-                      <Link iconOnly>
-                        <Icon material="bookmark" className="material-symbols-rounded"/>
-                      </Link>
-</div>
-                    )}
-                  </Link>
-                </CardContent>
-                }
-                
-                  <Block className="mt-4 mb-4">
-                    {post.likes_count > 0 &&
-<Block className="text-grey text-bold text-md">
-
-
-                  { post.likes_count+' Likes'}
-</Block>
-}
-
-                  </Block>
-            
-                <CardFooter className="rt-card-footer">
-                  <Button round>
-                    <Icon
-                      className="material-symbols-rounded"
-                      material="thumb_up"
-                    />
-                  </Button>
-                  <Button round>
-                    <Icon
-                      className="material-symbols-rounded"
-                      material="chat"
-                      />
-                  </Button>
-                  <Button round>
-                    <Icon
-                      className="material-symbols-rounded"
-                      md="material:share"
-                      ios="material:ios_share"
-                    />
-                  </Button>
-                </CardFooter>
-              </Card>
+             <PostCard key={index} post={post}/>
             );
           })}
 
